3372604c936eb4e967d525b785d3574f63c401d0
eduardoescuti <eescuti@fi.uba.ar>
2022-06-06 15:22:42 -0300

    Actualización del primer test.

--- src/main/java/edu/fiuba/algo3/modelo/Vehiculos/IVehiculo.java
+++ src/main/java/edu/fiuba/algo3/modelo/Vehiculos/IVehiculo.java
@@ -4,7 +4,7 @@ import edu.fiuba.algo3.modelo.Movimiento;
 
 public interface IVehiculo {
 
-   public Movimiento avanzarComun();
+    public Movimiento avanzarComun();
     public Movimiento avanzarPiquete();
     public Movimiento avanzarControlPolicial();
     public Movimiento avanzarPozo();
--- src/main/test/Entregas/CasosDeUsosEntrega1Tests.java
+++ src/main/test/Entregas/CasosDeUsosEntrega1Tests.java
@@ -1,6 +1,7 @@
 package Entregas;
 
 import edu.fiuba.algo3.modelo.Direccion;
+import edu.fiuba.algo3.modelo.Movimiento;
 import edu.fiuba.algo3.modelo.Obstaculo.Piquete;
 import edu.fiuba.algo3.modelo.Obstaculo.Pozo;
 import edu.fiuba.algo3.modelo.Vehiculos.Auto;
@@ -14,26 +15,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CasosDeUsosEntrega1Tests {
 
-    private Pozo obstaculo1;
+    private Pozo obstaculo1; // El pozo no creo que deba tener el contador de cuantas veces el jugador pasa por el mismo.
     private Piquete obstaculo2;
 
     IVehiculo cxc = new CuatroPorCuatro();
     IVehiculo moto = new Moto();
     IVehiculo auto = new Auto();
 
-
     Jugador jugador = new Jugador("Pepe");
     Direccion este = new Direccion();
 
-
-
-
-
     @Test
-    public void UnaMotoAvanzaYSeEncuentraConUnPozoSePenalizaEnTresMovimientos (){
+    public void UnaMotoAvanzaYSeEncuentraConUnPozoSePenalizaEnTresMovimientos (){ //¿Algo así debería quedar el test1?
 
         this.obstaculo1 = new Pozo(1);
-        //assertEquals(3, jugador.avanzarHaciaLaDireccion( este, moto ) );
+        Movimiento movimientos = jugador.avanzarHaciaLaDireccion( este, moto );
+        jugador.sumarMovimientos( movimientos );
+        assertEquals(3, jugador.obtenerPuntaje() );
     }
 
     @Test
