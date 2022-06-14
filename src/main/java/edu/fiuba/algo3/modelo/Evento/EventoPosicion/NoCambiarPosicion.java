6bceafc9d341adae4a2cbe2fb265d5c6ec8ad33b
Fernando Fraga <ferfraga2@gmail.com>
2022-06-14 12:34:25 -0300

    NoCambiarPosicionTest: OK

--- src/main/java/edu/fiuba/algo3/modelo/Evento/EventoPosicion/NoCambiarPosicion.java
+++ src/main/java/edu/fiuba/algo3/modelo/Evento/EventoPosicion/NoCambiarPosicion.java
@@ -1,11 +1,22 @@
 package edu.fiuba.algo3.modelo.Evento.EventoPosicion;
 
+import edu.fiuba.algo3.modelo.Celda.Celda;
+import edu.fiuba.algo3.modelo.Direccion.Direccion;
+
 public class NoCambiarPosicion implements EventoPosicion{
+    private Direccion direccion;
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion; // No es de utilidad.
+    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         return true;
+    }
+
+    public Celda actualizarPosicion(Celda celdaActual) {
+        return celdaActual;
     }
 }
--- src/main/test/edu/fiuba/algo3/modelo/Evento/EventoPosicion/NoCambiarPosicionTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/Evento/EventoPosicion/NoCambiarPosicionTest.java
@@ -1,7 +1,41 @@
 package edu.fiuba.algo3.modelo.Evento.EventoPosicion;
 
+import edu.fiuba.algo3.modelo.Celda.Celda;
+import edu.fiuba.algo3.modelo.Direccion.Direccion;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NoCambiarPosicionTest {
+    @Test
+    public void cualquieraSeaLaDireccionDevuelveLaMismaCeldaQueSePasaPorParametro(){
+        NoCambiarPosicion sinCambiar = new NoCambiarPosicion();
 
+        Celda celdaOeste = new Celda( null );
+        Celda celdaEste = new Celda( null );
+        Celda celdaNorte = celdaEste;
+        Celda celdaSur = celdaEste;
+
+        Celda celdaActual = new Celda( null, celdaNorte, celdaEste, celdaSur, celdaOeste );
+
+        Celda esperado = celdaActual;
+
+        sinCambiar.setDireccion(Direccion.ESTE);
+        Celda resultado = sinCambiar.actualizarPosicion( celdaActual );
+        assertEquals(esperado, resultado);
+
+
+        sinCambiar.setDireccion(Direccion.NORTE);
+        resultado = sinCambiar.actualizarPosicion( celdaActual );
+        assertEquals(esperado, resultado);
+
+        sinCambiar.setDireccion(Direccion.SUR);
+        resultado = sinCambiar.actualizarPosicion( celdaActual );
+        assertEquals(esperado, resultado);
+
+        sinCambiar.setDireccion(Direccion.OESTE);
+        resultado = sinCambiar.actualizarPosicion( celdaActual );
+        assertEquals(esperado, resultado);
+
+    }
 }
