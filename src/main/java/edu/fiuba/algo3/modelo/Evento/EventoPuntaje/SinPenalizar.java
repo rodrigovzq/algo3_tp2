e2ff606fbde1662050ae4fe3a64c853254f6a490
Fernando Fraga <ferfraga2@gmail.com>
2022-06-14 13:22:59 -0300

    SinPenalizarTest: OK

--- src/main/java/edu/fiuba/algo3/modelo/Evento/EventoPuntaje/SinPenalizar.java
+++ src/main/java/edu/fiuba/algo3/modelo/Evento/EventoPuntaje/SinPenalizar.java
@@ -3,9 +3,10 @@ package edu.fiuba.algo3.modelo.Evento.EventoPuntaje;
 import edu.fiuba.algo3.modelo.Movimiento.Movimiento;
 
 public class SinPenalizar implements EventoPuntaje {
+    public static final int MOVIMIENTO_DEFAULT = 1;
     @Override
     public Movimiento actualizarPuntaje(Movimiento m) {
-        return m;
+        return m.sumarMovimiento(new Movimiento(MOVIMIENTO_DEFAULT));
     }
 
     @Override
