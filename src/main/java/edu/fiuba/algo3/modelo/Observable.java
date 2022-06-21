b927289094e8597968cd3ffe2804608706ddf6c5
Fernando Fraga <ferfraga2@gmail.com>
2022-06-21 13:26:38 -0300

    Agrego la clase Observador y Observable. Util para la actualizacion de vistas.

---
+++ src/main/java/edu/fiuba/algo3/modelo/Observable.java
@@ -0,0 +1,24 @@
+package edu.fiuba.algo3.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Las clases pertenecientes al Modelo son observables y van a notificar la ocurrencia
+// de cambios de estados cada vez que se cambie algo en el.
+public abstract class Observable {
+    private List<Observador> observadores;
+
+    public Observable() {
+        this.observadores = new ArrayList<>();
+    }
+
+    public void notificarATodos(){
+        for( Observador o : observadores){
+            o.actualizar();
+        }
+    }
+
+    public void agregarObservador(Observador o){
+        observadores.add(o);
+    }
+}
--- src/main/java/edu/fiuba/algo3/modelo/Observador.java
+++ src/main/java/edu/fiuba/algo3/modelo/Observador.java
@@ -1,4 +1,8 @@
 package edu.fiuba.algo3.modelo;
 
+// Las vistas son observadores de los estados de las clases pertenecientes al Modelo
+//   Cada vez que se realize un cambio en una de las clases Modelo, se les notificará
+// el cambio y cada vista sabrá que datos pedir para actualizarse.
 public interface Observador {
+    void actualizar();
 }
