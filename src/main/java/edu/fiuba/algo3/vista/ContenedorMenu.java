f64f0dfe2e37ec5a9bd3565f66d40826e37fdbb9
Fernando Fraga <ferfraga2@gmail.com>
2022-06-21 09:19:45 -0300

    Agrego funcionalidad de Salir al programa y linkear todos los botones a distintas acciones.

--- src/main/java/edu/fiuba/algo3/vista/ContenedorMenu.java
+++ src/main/java/edu/fiuba/algo3/vista/ContenedorMenu.java
@@ -18,12 +18,20 @@ public class ContenedorMenu {
         botonJugar.setOnAction( e -> new ContenedorConfiguracion( this.stage )  );
 
         Button botonRanking = new Button("Ranking");
+        botonRanking.setOnAction( e -> new ContenedorRanking( this.stage ));
 
         Button botonInstruciones = new Button("Instrucciones");
         botonInstruciones.setOnAction( e -> ContenedorInstrucciones.mostrar());
 
         Button botonSalir = new Button("Salir");
+        stage.setOnCloseRequest( e -> {
+            e.consume(); // Borra el comportamiento default de cerrar la ventana.
+            closeProgram();
+        });
+        botonSalir.setOnAction( e -> closeProgram() );
+
         Button botonHelpPls = new Button("help pls");
+        botonHelpPls.setOnAction( e -> VentanaPopUp.mostrar("Help", "no se como ayudarte") );
 
         VBox root = new VBox();
         root.getChildren().addAll(botonJugar,botonRanking,botonInstruciones,botonSalir,botonHelpPls);
@@ -39,4 +47,10 @@ public class ContenedorMenu {
         this.stage.show();
     }
 
+    private void closeProgram(){
+        boolean resultado = VentanaConfirmacion.mostrar( "Salir del juego", "¿Estas seguro de salir del juego?");
+        if( resultado ){
+            this.stage.close();
+        }
+    }
 }
