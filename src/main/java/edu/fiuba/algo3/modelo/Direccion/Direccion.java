6e9c5aa63861e113ef08271030b3882124927f74
Fernando Fraga <ferfraga2@gmail.com>
2022-06-23 22:56:26 -0300

    Agrego direcciones diagonales para la creacion de CeldaEsquina

--- src/main/java/edu/fiuba/algo3/modelo/Direccion/Direccion.java
+++ src/main/java/edu/fiuba/algo3/modelo/Direccion/Direccion.java
@@ -5,6 +5,10 @@ public enum Direccion {
         ESTE  { @Override public Direccion opuesto() { return OESTE;  }},
         SUR { @Override public Direccion opuesto() { return NORTE; }},
         OESTE  { @Override public Direccion opuesto() { return ESTE;  }},
+        NORESTE { @Override public Direccion opuesto() { return SUDOESTE; }},
+        NOROESTE  { @Override public Direccion opuesto() { return SUDESTE;  }},
+        SUDESTE { @Override public Direccion opuesto() { return NOROESTE; }},
+        SUDOESTE  { @Override public Direccion opuesto() { return NORESTE;  }},
         ;
         abstract public Direccion opuesto();
 }
