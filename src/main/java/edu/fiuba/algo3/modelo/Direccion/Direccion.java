dfb96a6dc0eca70eacb165a3195d7412168f0190
Fernando Fraga <ferfraga2@gmail.com>
2022-06-23 22:31:34 -0300

    ADD: Direccion.opuesto()

--- src/main/java/edu/fiuba/algo3/modelo/Direccion/Direccion.java
+++ src/main/java/edu/fiuba/algo3/modelo/Direccion/Direccion.java
@@ -1,5 +1,11 @@
 package edu.fiuba.algo3.modelo.Direccion;
 
-public enum Direccion {
-    NORTE, ESTE, SUR, OESTE
+public enum Direccion {     
+        NORTE { @Override public Direccion opuesto() { return SUR; }},
+        ESTE  { @Override public Direccion opuesto() { return OESTE;  }},
+        SUR { @Override public Direccion opuesto() { return NORTE; }},
+        OESTE  { @Override public Direccion opuesto() { return ESTE;  }},
+        ;
+        abstract public Direccion opuesto();
 }
+
