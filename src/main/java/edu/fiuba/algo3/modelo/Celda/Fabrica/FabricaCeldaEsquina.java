c09b34f89f9a8704bf6dd9912549b2200eb413b8
Fernando Fraga <ferfraga2@gmail.com>
2022-06-23 23:05:34 -0300

    FabricaEsquinaTest: OK

--- src/main/java/edu/fiuba/algo3/modelo/Celda/Fabrica/FabricaCeldaEsquina.java
+++ src/main/java/edu/fiuba/algo3/modelo/Celda/Fabrica/FabricaCeldaEsquina.java
@@ -1,4 +1,26 @@
 package edu.fiuba.algo3.modelo.Celda.Fabrica;
 
-public class FabricaCeldaEsquina {
+import edu.fiuba.algo3.modelo.Celda.Celda;
+import edu.fiuba.algo3.modelo.Celda.CeldaEsquina;
+import edu.fiuba.algo3.modelo.Direccion.Direccion;
+import edu.fiuba.algo3.modelo.Excepcion.DireccionInvalida;
+
+public class FabricaCeldaEsquina implements FabricaCelda{
+    @Override
+    public Celda crearCelda(Direccion dir) {
+        Celda celda = null;
+        if( dir == Direccion.NORESTE){
+            celda = new CeldaEsquina( null , Direccion.NORTE, Direccion.ESTE );
+        }else if( dir == Direccion.SUDESTE){
+            celda = new CeldaEsquina( null , Direccion.SUR, Direccion.ESTE );
+        }else if( dir == Direccion.NOROESTE){
+            celda = new CeldaEsquina( null , Direccion.NORTE, Direccion.ESTE );
+        }else if( dir == Direccion.SUDOESTE){
+            celda = new CeldaEsquina( null , Direccion.SUR, Direccion.OESTE );
+        }else{
+            throw new DireccionInvalida("creaCelda :");
+        }
+
+        return celda;
+    }
 }
--- src/main/test/edu/fiuba/algo3/modelo/Celda/Fabrica/FabricaCeldaEsquinaTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/Celda/Fabrica/FabricaCeldaEsquinaTest.java
@@ -1,7 +1,32 @@
 package edu.fiuba.algo3.modelo.Celda.Fabrica;
 
+import edu.fiuba.algo3.modelo.Celda.Celda;
+import edu.fiuba.algo3.modelo.Celda.CeldaEsquina;
+import edu.fiuba.algo3.modelo.Direccion.Direccion;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FabricaCeldaEsquinaTest {
+    @Test
+    public void creoUnaCeldaBordePasandoLaDireccionBloqueada(){
+        FabricaCeldaEsquina fabrica = new FabricaCeldaEsquina();
+        // TODO: ¿Debo pasarle el estado? El null no es correcto ya que no es un estado valido.
+        Celda norteste = fabrica.crearCelda(Direccion.NORESTE);
+        Celda sureste = fabrica.crearCelda(Direccion.SUDESTE);
+        Celda noroeste = fabrica.crearCelda(Direccion.NOROESTE);
+        Celda sudoeste = fabrica.crearCelda(Direccion.SUDOESTE);
 
+        Celda esperado = new CeldaEsquina(null, Direccion.NORTE,Direccion.ESTE);
+        assertEquals(esperado, norteste);
+
+        esperado = new CeldaEsquina(null, Direccion.SUR,Direccion.ESTE);
+        assertEquals(esperado, sureste);
+
+        esperado = new CeldaEsquina(null, Direccion.NORTE,Direccion.OESTE);
+        assertEquals(esperado, noroeste);
+
+        esperado = new CeldaEsquina(null, Direccion.SUR,Direccion.OESTE);
+        assertEquals(esperado, sudoeste);
+    }
 }
