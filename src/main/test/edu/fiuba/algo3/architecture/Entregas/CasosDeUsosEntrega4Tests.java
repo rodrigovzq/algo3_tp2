62bb1290aa19eade25be2eefaf03a99d6d5412c8
Fernando Fraga <ferfraga2@gmail.com>
2022-06-30 23:44:48 -0300

    REFACTOR: Test4

--- src/main/test/edu/fiuba/algo3/architecture/Entregas/CasosDeUsosEntrega4Tests.java
+++ src/main/test/edu/fiuba/algo3/architecture/Entregas/CasosDeUsosEntrega4Tests.java
@@ -1,5 +1,6 @@
 package edu.fiuba.algo3.architecture.Entregas;
 
+import edu.fiuba.algo3.modelo.Celda.Celda;
 import edu.fiuba.algo3.modelo.Direccion.Direccion;
 import edu.fiuba.algo3.modelo.Evento.Evento;
 import edu.fiuba.algo3.modelo.Jugador.Jugador;
@@ -14,15 +15,11 @@ public class CasosDeUsosEntrega4Tests {
     public void seGeneraUnMapaAleatoriamenteMeMuevoEnCirculoConElAutoYTengoMasMovimientosQueAvances(){
         //TODO: Mockear. Para quitar lo estocástico.
         //TODO: Testear generadores aleatorios.
-        //TODO: Implementar sortearCeldaJugador.
-        Mapa mapa1 = new Mapa( 20, 20);
-        mapa1.generarMapa();
-        mapa1.sortearEstadosMapa();
-        Jugador jugador1 = new Jugador( "Grupo3", mapa1.sortearCeldaJugador(), new Auto());
-
-        Mapa mapa2 = new Mapa( 20, 20);
-        mapa2.generarMapa();
-        Jugador jugador2 = new Jugador( "Grupo3", mapa2.sortearCeldaJugador(), new Auto());
+        Mapa mapa = new Mapa( 20, 20);
+        mapa.generarMapa();
+        Celda posicionJugador = mapa.sortearCeldaJugador();
+        Jugador jugador1 = new Jugador( "Grupo3", posicionJugador, new Auto());
+        Jugador jugador2 = new Jugador( "Grupo3", posicionJugador, new Auto());
 
         Evento ev = jugador1.avanzarHaciaLaDireccion(Direccion.ESTE);
         jugador1.actualizar( ev );
@@ -32,6 +29,8 @@ public class CasosDeUsosEntrega4Tests {
         jugador1.actualizar( ev );
         ev = jugador1.avanzarHaciaLaDireccion(Direccion.NORTE);
         jugador1.actualizar( ev );
+
+        mapa.sortearEstadosMapa();
 
         ev = jugador2.avanzarHaciaLaDireccion(Direccion.ESTE);
         jugador2.actualizar( ev );
