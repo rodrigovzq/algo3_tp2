bc3e5814fa02aad3b44691423bfa8ad2188e919a
Fernando Fraga <ferfraga2@gmail.com>
2022-07-02 17:49:13 -0300

    Sorpresa: Imprimibles

--- src/main/java/edu/fiuba/algo3/modelo/Sorpresa/CambioVehiculo.java
+++ src/main/java/edu/fiuba/algo3/modelo/Sorpresa/CambioVehiculo.java
@@ -1,6 +1,6 @@
 package edu.fiuba.algo3.modelo.Sorpresa;
 
-import edu.fiuba.algo3.modelo.Celda.EstadoCelda;
+import edu.fiuba.algo3.modelo.EstadoCelda.EstadoCelda;
 import edu.fiuba.algo3.modelo.Evento.Evento;
 import edu.fiuba.algo3.modelo.Evento.EventoPosicion.Avanzar;
 import edu.fiuba.algo3.modelo.Evento.EventoPuntaje.SinPenalizar;
@@ -11,5 +11,9 @@ public class CambioVehiculo implements EstadoCelda {
     @Override
     public Evento avanzarEn(IVehiculo vehiculo) {
         return new Evento(new SinPenalizar(), new SiguienteVehiculo(), new Avanzar());
+    }
+    @Override
+    public String imprimir() {
+        return Sorpresa.CAMBIO_VEHICULO.name();
     }
 }
