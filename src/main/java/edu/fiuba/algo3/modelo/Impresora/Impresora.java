e321e7bd8f51bd8f37c73ba097991ae63ef5a9da
Fernando Fraga <ferfraga2@gmail.com>
2022-07-02 17:45:21 -0300

    Impresora: imprimir(Imprimible)

--- src/main/java/edu/fiuba/algo3/modelo/Impresora/Impresora.java
+++ src/main/java/edu/fiuba/algo3/modelo/Impresora/Impresora.java
@@ -1,7 +1,11 @@
 package edu.fiuba.algo3.modelo.Impresora;
 
+import edu.fiuba.algo3.modelo.Juego.Ranking;
+
 public interface Impresora {
     void escribirNuevaLinea(String s);
 
     void finalizarEscritura();
+
+    void imprimir(Imprimible elemento);
 }
--- src/main/java/edu/fiuba/algo3/modelo/Impresora/Imprimible.java
+++ src/main/java/edu/fiuba/algo3/modelo/Impresora/Imprimible.java
@@ -1,6 +1,5 @@
 package edu.fiuba.algo3.modelo.Impresora;
 
 public interface Imprimible {
-
     String imprimir();
 }
