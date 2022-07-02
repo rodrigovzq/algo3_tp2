54b111b599c00e7a6d377a3af6c10de821735ab6
Fernando Fraga <ferfraga2@gmail.com>
2022-07-02 17:45:39 -0300

    metodos de persistencia

--- src/main/java/edu/fiuba/algo3/modelo/Juego/Juego.java
+++ src/main/java/edu/fiuba/algo3/modelo/Juego/Juego.java
@@ -5,6 +5,8 @@ import edu.fiuba.algo3.modelo.Impresora.ImpresoraFile;
 import edu.fiuba.algo3.modelo.Jugador.Jugador;
 import edu.fiuba.algo3.modelo.Lector.Lector;
 import edu.fiuba.algo3.modelo.Lector.LectorScanner;
+import edu.fiuba.algo3.modelo.Mapa.Mapa;
+import edu.fiuba.algo3.modelo.Observable;
 import edu.fiuba.algo3.modelo.Observador;
 
 import java.util.ArrayList;
@@ -18,9 +20,9 @@ public class Juego implements Observador {
     private final Ranking ranking;
 
     public Juego( Jugador jugador) {
-        this.lector = new LectorScanner("saves/ranking.txt");
         this.jugador = jugador;
         this.impresora = new ImpresoraFile("saves/ranking.txt");
+        this.lector = new LectorScanner("saves/ranking.txt");
         List<String> lista = this.lecturaPuntaje();
         this.ranking = new Ranking(lista);
     }
@@ -36,9 +38,30 @@ public class Juego implements Observador {
         return lista;
     }
 
+    //Al llegar a la meta, se le notifica que debe agregar al jugador.
     @Override
     public void actualizar() {
         this.ranking.agregar(jugador);
-//        this.impresora.imprimir( ranking );
+        this.guardarRanking();
     }
+
+    private void guardarRanking() {
+        this.impresora = new ImpresoraFile("saves/ranking.txt");
+        this.impresora.imprimir( ranking );
+    }
+
+    public void guardarMapa(){
+        this.impresora = new ImpresoraFile("saves/mapa.txt");
+    }
+    public void guardarJugador(){
+        this.impresora = new ImpresoraFile("saves/jugador.txt");
+        this.impresora.imprimir( jugador );
+    }
+    //TODO: ¿O es más correcto que el juego tenga una instancia del Mapa?
+    public void guardarMapa( Mapa mapa ){
+        this.impresora = new ImpresoraFile("saves/jugador.txt");
+        this.impresora.imprimir( mapa );
+    }
+
+    //TODO: Guardar meta!!
 }
