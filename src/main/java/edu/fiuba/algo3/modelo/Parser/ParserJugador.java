d67f57f4e1cae8b56e66e2c4a6590806ccbe0bdf
Fernando Fraga <ferfraga2@gmail.com>
2022-07-04 12:29:52 -0300

    ParserJugador: OK. Falta referenciar a la celda del mapa

--- src/main/java/edu/fiuba/algo3/modelo/Parser/ParserJugador.java
+++ src/main/java/edu/fiuba/algo3/modelo/Parser/ParserJugador.java
@@ -1,4 +1,63 @@
 package edu.fiuba.algo3.modelo.Parser;
 
-public class ParserJugador {
+import edu.fiuba.algo3.modelo.Celda.Celda;
+import edu.fiuba.algo3.modelo.Celda.FabricaCelda.FabricaCeldaInterna;
+import edu.fiuba.algo3.modelo.Coordenada.Coordenada;
+import edu.fiuba.algo3.modelo.Excepcion.ArchivoMalformado;
+import edu.fiuba.algo3.modelo.Jugador.Jugador;
+import edu.fiuba.algo3.modelo.Lector.LectorScanner;
+import edu.fiuba.algo3.modelo.Vehiculos.IVehiculo;
+import edu.fiuba.algo3.modelo.Vehiculos.Vehiculo;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ParserJugador implements Parser {
+    private final String PATRON_NOMBRE = "(\\w+\\s?\\w*)";
+    private final String PATRON_UBICACION = "\\((\\d+),(\\d+)\\)";
+    private final String PATRON_VEHICULO = "(\\w+)";
+    private final String PATRON_PUNTAJE = "(\\d+)+";
+    private final String texto;
+
+    public ParserJugador(String pathFile) {
+        this.texto = new LectorScanner(pathFile).leerTodoElArchivo();
+
+    }
+
+    public Jugador parsear() {
+        String patron_grupo =  PATRON_NOMBRE + Jugador.DELIMITADOR +  PATRON_UBICACION + Jugador.DELIMITADOR +  PATRON_VEHICULO  + Jugador.DELIMITADOR  + PATRON_PUNTAJE ;
+        Pattern patron = Pattern.compile( patron_grupo );
+        Matcher matcher = patron.matcher(this.texto);
+
+        String nombreJugador;
+        IVehiculo vehiculoJugador;
+        Celda posicionJugador;
+        Integer puntajeJugador;
+        FabricaCeldaInterna fabrica = new FabricaCeldaInterna();
+
+        //En caso de que exista el archivo y este bien formado
+        if( matcher.find() ){
+            nombreJugador = matcher.group(1);
+
+            //El problema es que leemos una coordenada y necesitamos la celda.
+            //(!!!!) Necesitamos tener el mapa ya formado.
+            Integer posX = Integer.parseInt( matcher.group(2) );
+            Integer posY = Integer.parseInt( matcher.group(3) );
+            Coordenada coord = new Coordenada(posX,posY);
+
+            //TODO: Como leer la ubicacion y dar la celda del mapa.
+            posicionJugador = fabrica.crearCelda(coord);
+
+            String vehiculo = matcher.group(4);
+            vehiculoJugador = Vehiculo.crearVehiculoDesdeString(vehiculo);
+
+            puntajeJugador = Integer.parseInt( matcher.group(5) );
+
+        }else{
+            throw new ArchivoMalformado();
+        }
+
+        return new Jugador(nombreJugador, posicionJugador, vehiculoJugador, puntajeJugador);
+    }
+
 }
