fe0e3366dbb3103168ea3686d594864d750bd8f7
Fernando Fraga <ferfraga2@gmail.com>
2022-07-04 12:32:47 -0300

    ParserJugador: OK

--- src/main/test/edu/fiuba/algo3/modelo/Parser/ParserJugadorTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/Parser/ParserJugadorTest.java
@@ -1,7 +1,26 @@
 package edu.fiuba.algo3.modelo.Parser;
 
+import edu.fiuba.algo3.modelo.Celda.CeldaInterna;
+import edu.fiuba.algo3.modelo.Coordenada.Coordenada;
+import edu.fiuba.algo3.modelo.EstadoCelda.Comun;
+import edu.fiuba.algo3.modelo.Jugador.Jugador;
+import edu.fiuba.algo3.modelo.Vehiculos.Auto;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParserJugadorTest {
+    @Test
+    public void leeDeUnArchivoYDevuelveElJugador(){
+        String pathFileTest = "saves/test/jugador_test.txt";
+        ParserJugador parser = new ParserJugador( pathFileTest );
+
+        Jugador resultado = parser.parsear();
+        //TODO: De alguna forma hay que inicializarlo con una celda de mapa
+        Jugador esperado = new Jugador("Test", new CeldaInterna(new Comun(), new Coordenada(3,4)),new Auto(),100);
+
+        assertEquals(esperado, resultado);
+
+    }
 
 }
