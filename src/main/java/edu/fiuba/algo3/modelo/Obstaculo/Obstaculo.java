a985d97eea273d5af0ed920abe3901e2ac2355a4
Fernando Fraga <ferfraga2@gmail.com>
2022-07-04 12:29:23 -0300

    Obstaculo.crear()

--- src/main/java/edu/fiuba/algo3/modelo/Obstaculo/Obstaculo.java
+++ src/main/java/edu/fiuba/algo3/modelo/Obstaculo/Obstaculo.java
@@ -1,5 +1,24 @@
 package edu.fiuba.algo3.modelo.Obstaculo;
 
+import edu.fiuba.algo3.modelo.GeneradorAleatorio.GeneradorDemora;
+
 public enum Obstaculo {
-    CONTROL_POLICIAL, PIQUETE, POZO
+    CONTROL_POLICIAL{
+        @Override
+        public IObstaculo crear(){ return new ControlPolicial(new GeneradorDemora());}
+    },
+    PIQUETE{
+        @Override
+        public IObstaculo crear(){ return new Piquete();}
+    },
+    POZO{
+        @Override
+        public IObstaculo crear(){ return new Pozo();}
+    };
+
+    public static IObstaculo crearObstaculoDesdeString(String s){
+        return valueOf( s.toUpperCase() ).crear();
+    }
+
+    public abstract IObstaculo crear();
 }
