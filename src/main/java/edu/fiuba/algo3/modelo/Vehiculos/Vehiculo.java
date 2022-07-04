3014924bc1fc37a1b31d912ead66bddd5e7c12b3
Fernando Fraga <ferfraga2@gmail.com>
2022-07-04 12:32:06 -0300

    Vehiculo.crear(): IVehiculo

--- src/main/java/edu/fiuba/algo3/modelo/Vehiculos/Vehiculo.java
+++ src/main/java/edu/fiuba/algo3/modelo/Vehiculos/Vehiculo.java
@@ -1,4 +1,21 @@
 package edu.fiuba.algo3.modelo.Vehiculos;
 
 public enum Vehiculo {
+    AUTO{
+        @Override
+        public IVehiculo crear(){ return new Auto();}
+    },
+    MOTO{
+        @Override
+         public IVehiculo crear(){ return new Moto();}
+    },
+    CUATROPORCUATRO{
+        @Override
+        public IVehiculo crear(){ return new CuatroPorCuatro();}
+    };
+
+    public abstract IVehiculo crear();
+    public static IVehiculo crearVehiculoDesdeString(String s){
+        return valueOf( s.toUpperCase() ).crear();
+    }
 }
